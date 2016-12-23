f9825c404e45c4797f635777998bc45072b5f1ab
Etudiant <Etudiant>
2016-12-23 12:30:23 +0100

    Ajout PHP/HTML

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+
+// Code PHP ....
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Boutique Pokemon</title>
+</head>
+<body>
+	<h1>PokeFan Store</h1>
+	<p>Bienvenue sur la premiere boutique Pokemon</p>
+
+	<nav>
+		<ul>
+			<li>Accueil</li>
+			<li>Articles</li>
+			<li>Contact</li>
+		</ul>
+	</nav>
+</body>
+</html>
